bd930da08d07c6ebe2fc5c8a9dbd0638c654b772
Leekm0912 <42348176+Leekm0912@users.noreply.github.com>
2021-09-02 22:17:07 +0900

    일단 저장. 내일하자

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import cv2, dlib  # 이미지 표현 및 눈 좌표 얻어오기
-import numpy as np  # 데이터 처리
-from imutils import face_utils  # 얼굴 분석
-from tensorflow import keras  # 모델 학습 및 테스트
-from playsound import playsound  # 소리 재생
-import threading  # 스레드 사용
+import time
 
-cap = cv2.VideoCapture(0)
+import cv2 as cv
+
+from PostureDetection import PostureDetection
+
+print(cv.__version__)
+print(cv.cuda.getCudaEnabledDeviceCount())
+
+cap = cv.VideoCapture(0)
+time.sleep(2)
+pd = PostureDetection(cap)
 while cap.isOpened():
-    hasFrame, frame = cap.read()
-    if not hasFrame:
-        cv2.waitKey()
-        break
 
-    if cv2.waitKey(1) == ord('q'):
+    frame = pd.detection()
+    cv.imshow('OpenPose using OpenCV', frame)
+
+    if cv.waitKey(1) == ord('q'):
         break
