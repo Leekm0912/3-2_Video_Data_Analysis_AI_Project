69e6ae8cfcf40ec54574ed5cdba28136bee54482
Leekm0912 <42348176+Leekm0912@users.noreply.github.com>
2021-09-13 09:24:48 +0900

    기능 사용 관련 config 추가

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def work_thread(cap, posture_detection_obj, eye_detection_obj, status_check_obj,
         # eye_detection_image = eye_detection_obj.detection(frame, status_check_obj)
 
         # 패널이 None이면 pack 시켜줌
-        if posture_detection_obj.result_frame:
+        if use_postureDetection and posture_detection_obj.result_frame:
             if posture_detection_image_panel is None:
                 posture_detection_image_panel = tk.Label(image=posture_detection_obj.result_frame)
                 posture_detection_image_panel.image = posture_detection_obj.result_frame
@@ -37,7 +37,7 @@ def work_thread(cap, posture_detection_obj, eye_detection_obj, status_check_obj,
                 posture_detection_image_panel.configure(image=posture_detection_obj.result_frame)
                 posture_detection_image_panel.image = posture_detection_obj.result_frame
         # 동일
-        if eye_detection_obj.result_frame:
+        if use_eyeDetection and eye_detection_obj.result_frame:
             if eye_detection_image_panel is None:
                 eye_detection_image_panel = tk.Label(image=eye_detection_obj.result_frame)
                 eye_detection_image_panel.image = eye_detection_obj.result_frame
@@ -72,6 +72,10 @@ if __name__ == '__main__':
     except ValueError:
         input_source = config["input"]["input"]
 
+    use_eyeDetection = int(config["GUI"]["use_eyeDetection"])
+    use_postureDetection = int(config["GUI"]["use_postureDetection"])
+    use_graph = int(config["GUI"]["use_graph"])
+
     # 카메라 동작
     _cap = cv.VideoCapture(input_source)
 
@@ -83,10 +87,11 @@ if __name__ == '__main__':
     status_label.pack(side="bottom")
 
     # 그래프
-    mg = MakeGraph.MakeGraph(root, status_check)
-    graph = threading.Thread(target=mg.start_graph, args=())
-    graph.daemon = True
-    graph.start()
+    if use_graph:
+        mg = MakeGraph.MakeGraph(root, status_check)
+        graph = threading.Thread(target=mg.start_graph, args=())
+        graph.daemon = True
+        graph.start()
 
     # thread 시작
     thread_img = threading.Thread(target=work_thread, args=(_cap, posture_detection, eye_detection, status_check, status_text, status_label))
